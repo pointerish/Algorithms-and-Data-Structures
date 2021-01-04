c68e036f1d63dc8b391940653fc2d24521da1a17
Josias Alvarado <josiasjag@gmail.com>
2021-01-04 12:02:38 -0600

    Add methods for Queue class.

--- Queue/queue.rb
+++ Queue/queue.rb
@@ -1,9 +1,15 @@
-require_relative '../LinkedList/SinglyLinkedList/singly_linked_list'
+require '../LinkedList/SinglyLinkedList/singly_linked_list'
 
 class Queue < SinglyLinkedList
   
   def enqueue(obj)
     new_head = Node.new(obj, head)
     head = new_head
+  end
+
+  def dequeue
+    item = head
+    remove(0)
+    item
   end
 end
